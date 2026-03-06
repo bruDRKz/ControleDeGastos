85399962707241a944019bea40401d5ac3b1ef43
Bruno Carvalho Rodrigues <br.rodrigues373@gmail.com>
2026-03-05 22:56:06 -0300

    Criação repository de Pessoas

---
+++ Backend/src/ControleDeGastos.Domain/Repositories/PessoaRepository/IPessoaRepository.cs
@@ -0,0 +1,13 @@
+﻿using ControleDeGastos.Domain.Entities;
+
+namespace ControleDeGastos.Domain.Repositories.PessoaRepository
+{
+    public interface IPessoaRepository
+    {
+            Task<Pessoa?> ObterPorIdAsync(int id);
+            Task<List<Pessoa>> ObterTodosAsync();
+            Task AdicionarAsync(Pessoa pessoa);
+            Task AtualizarAsync(Pessoa pessoa);
+            Task ExcluirAsync(int id);
+    }
+}
