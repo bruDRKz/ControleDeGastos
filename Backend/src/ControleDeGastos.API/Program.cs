d41195abbe182a064110886ec7cb71209e22702f
Bruno Carvalho Rodrigues <br.rodrigues373@gmail.com>
2026-03-08 00:29:00 -0300

    Configuração do Swagger

--- Backend/src/ControleDeGastos.API/Program.cs
+++ Backend/src/ControleDeGastos.API/Program.cs
@@ -1,13 +1,19 @@
+using ControleDeGastos.Application;
 using ControleDeGastos.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -17,7 +23,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
